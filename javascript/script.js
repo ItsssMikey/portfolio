a8fcef89985e8fba18c2c3b6fbd7e2e526053f6c
michaelhuse <s375119@oslomet.no>
2025-04-01 23:50:47 +0200

    first commit

---
+++ javascript/script.js
@@ -0,0 +1,16 @@
+
+            // Dark Mode Toggle Functionality
+            function myFunction() {
+              const body = document.body;
+              body.classList.toggle("dark-mode");
+
+              // Change icon to reflect mode
+              const icon = document.querySelector("#dark-mode span");
+              if (body.classList.contains("dark-mode")) {
+                  icon.classList.remove("fa-circle-half-stroke");
+                  icon.classList.add("fa-sun");
+              } else {
+                  icon.classList.remove("fa-sun");
+                  icon.classList.add("fa-circle-half-stroke");
+              }
+          }
